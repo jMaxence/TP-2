6dfd46a4506301e2d86dcbf82bee771eedc6a775
Jolibois <90181722+jMaxence@users.noreply.github.com>
2021-09-21 11:31:03 +0200

    Add files via upload

---
+++ exo3/ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,34 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+	int poids = 0;
+	
+
+	printf("entre le poids");
+	scanf("%d", &poids);
+
+	if (poids < 20) {
+
+		printf("prix est de 3.00frs");
+	}
+
+	if (poids >20 && poids<= 50) {
+
+		printf("prix est de 3.95frs");
+	}
+
+	if (poids >50) {
+
+		printf("prix est de 5.90frs");
+
+	}
+}
+
+		
+	
