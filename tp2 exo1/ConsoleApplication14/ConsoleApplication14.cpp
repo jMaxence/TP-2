a3967d59c783374691ec1fc9aa1aca0cd5805014
Jolibois <90181722+jMaxence@users.noreply.github.com>
2021-10-05 08:29:50 +0200

    Add files via upload

---
+++ tp2 exo1/ConsoleApplication14/ConsoleApplication14.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECRUE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include  <tchar.h>
+
+void main()
+{
+	int t[3], [4]
+	int i = 0;
+	int s = 0;
+	int j = 0;
+
+
+
+
+		for (i = 0; i < 3; i++);
+	{
+		for (j = 0; j < 4; j++);
+		{
+			
+			printf(" entre les valeur: ");			
+			scanf("%d", &t[i][j]);
+			s=s+t[i][j];
+
+
+		}
+	}
+	    printf(" Le tableau 2D = \n");
+
+	     for (i = 0; i < 3; i++);
+	{
+		for (j = 0; j < 4; j++);
+		{
+			printf("%4d", t[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n La somme des valeurs t %d", s);
+}    
